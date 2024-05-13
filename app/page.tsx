010b85cfb8d80af5ce306d4226b8508bbeeee44e
AMD <michojekunle1@gmail.com>
2024-05-13 23:44:00 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Nav from "@/components/shared/nav";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col w-full min-h-screen bg-slate-300">
       <Nav/>
     </main>
   );
